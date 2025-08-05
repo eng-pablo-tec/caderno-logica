f4716554a547ec018c0f6ca4bb72ce86be774cb8
Pablo Henrique Lima dos Santos <136536368+eng-pablo-tec@users.noreply.github.com>
2025-08-04 21:58:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function mostrar(id) {
-  const telas = document.querySelectorAll('.tela');
-  telas.forEach(tela => tela.style.display = 'none');
-  document.getElementById(id).style.display = 'block';
-}
 
-// Mostra a primeira tela ao abrir
-mostrar('introducao');
